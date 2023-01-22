ce6f1b16bc92dcd7b9d19eb941676cfd5e275069
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2023-01-22 18:41:27 -0500

    default credentials

--- project/login/forms.py
+++ project/login/forms.py
@@ -34,11 +34,13 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField(
         validators=[InputRequired(), Length(min=4, max=20)],
+        default="admin",
         render_kw={"placeholder": "Username"},
     )
 
     password = PasswordField(
         validators=[InputRequired(), Length(min=8, max=20)],
+        default="admin@123",
         render_kw={"placeholder": "Password"},
     )
 
